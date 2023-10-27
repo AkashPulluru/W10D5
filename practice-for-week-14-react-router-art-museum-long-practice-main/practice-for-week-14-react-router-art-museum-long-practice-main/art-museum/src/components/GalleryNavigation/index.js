aaa925f3e2684c7c0b8a47b35058efdf8f4ec257
Francis Cawog <Franciscawog@gmail.com>
2023-10-27 15:21:52 -0400

    step 10

---
+++ practice-for-week-14-react-router-art-museum-long-practice-main/practice-for-week-14-react-router-art-museum-long-practice-main/art-museum/src/components/GalleryNavigation/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+function GalleryNavigation({ galleries }) {
+    return (
+        <nav>
+        <h1>Hello from App</h1>
+        <ul>
+            <li>
+            <NavLink exact to="/">Home</NavLink>
+            </li>
+            {galleries.map(gallery => (
+            <li key={gallery.id}>
+                <NavLink to={`/galleries/${gallery.id}`}>
+                {gallery.name}
+                </NavLink>
+            </li>
+            ))}
+        </ul>
+        </nav>
+    );
+}
+
+export default GalleryNavigation;
